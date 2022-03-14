27811ce6282aa5148911d9d23f75d3e245bd6ef9
LiuShihao <1692312138@qq.com>
2022-03-14 14:13:21 +0800

    HelloWord监听消息

---
+++ src/main/java/com/lsh/Consumer.java
@@ -0,0 +1,42 @@
+package com.lsh;
+
+import com.rabbitmq.client.*;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * @author ：LiuShihao
+ * @date ：Created in 2022/3/14 2:07 下午
+ * @desc ：监听队列 进行消费
+ */
+public class Consumer {
+    public static final String QUEUE_NAME = "hello";
+
+    /**
+     * 发送消息
+     */
+    @Test
+    public void publisher() throws Exception {
+        // 1.获取连接对象
+        Connection connection = RabbitMQConnectionUtil.getConnection();
+
+        //2.构建Channel
+        Channel channel = connection.createChannel();
+
+        //3.构建队列
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+        //4.监听队列
+        DefaultConsumer callback = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println("消费者获取到消息:" + new String(body, "UTF-8"));
+            }
+        };
+        //
+        channel.basicConsume(QUEUE_NAME,true,callback);
+        System.out.println("开始监听队列");
+        System.in.read();
+    }
+}
